9994887808edcf471d66beeadc4003a543215526
Benny Tieu <benny.tieu@gmail.com>
2021-12-24 18:55:50 +0100

    Add solution day 6 part 2.
    Part 1 was quite naive... instead of representing the
    LanternFishes as a list and iterate through it each day
    (Heap size will be emptied), we use a Map,
    where the key is internal timer
    and the value is the number of fishes with that specific
    internal timer

--- src/main/kotlin/day6/Main.kt
+++ src/main/kotlin/day6/Main.kt
@@ -1,32 +1,54 @@
-import day6.LanternFish
 import java.io.File
 
 fun main (){
     val internalTimers = File("src/main/kotlin/day6/input").readLines()[0].split(",").map { it.toInt() }
 
-    val lanternFishes = internalTimers.map { LanternFish(it) }
+    val lanternFishesMapPart1 = createLanternFishesMap(internalTimers)
+    val lanternFishesMapPart2 = createLanternFishesMap(internalTimers)
 
-    println("Answer part 1: ${simulate80Days(lanternFishes).size}")
+    for (i in 1 .. 80) {
+        lanternFishesMapPart1.simulate()
+    }
+
+    for (i in 1 .. 256) {
+        lanternFishesMapPart2.simulate()
+    }
+
+    // 362346
+    println("Answer part 1: ${lanternFishesMapPart1.getFishCount()}")
+
+    // 1639643057051
+    println("Answer part 2: ${lanternFishesMapPart2.getFishCount()}")
 
 }
 
-fun simulate80Days(lanternFishes: List<LanternFish>): List<LanternFish> {
-    val mutLanternFishes = lanternFishes.toMutableList()
-    for (i in 1 .. 80) {
-        val additionalFishes = mutableListOf<LanternFish>()
-        for (fish in mutLanternFishes) {
-            if (fish.internalTimer == 0) {
-                additionalFishes.add(LanternFish())
-                fish.resetInternalTimer()
-            } else {
-                fish.decreaseInternalDay()
-            }
-        }
-        mutLanternFishes.addAll(additionalFishes)
+// Key = Internal Timer k in Key (where k is [0,1,..,8])
+// Value = The number of LanternFishes
+fun createLanternFishesMap(internalTimers: List<Int>): MutableMap<Int, Long> {
+    val lanternFishesMap = mutableMapOf<Int, Long>()
 
-//      print("Day $i: ")
-//      println(mutLanternFishes.forEach{ print(""+it.internalTimer + ",")})
+    internalTimers.forEach { fish ->
+        val count = lanternFishesMap.getOrDefault(fish, 0)
+        lanternFishesMap[fish] = count + 1
     }
 
-    return mutLanternFishes
+    return lanternFishesMap
+}
+
+fun MutableMap<Int, Long>.getFishCount(): Long {
+    return values.sum()
+}
+
+fun MutableMap<Int, Long>.simulate() {
+    val additionalFishes = get(0) ?: 0
+
+    for (i in 1 .. 8) {
+        val internalTime = i-1
+        val internalTimeCount = get(i) ?: 0
+        put(internalTime, internalTimeCount)
+    }
+
+    put(8, additionalFishes)
+    val numOfFishesWithInternalTime6 = get(6) ?: 0
+    put(6, numOfFishesWithInternalTime6 + additionalFishes)
 }
